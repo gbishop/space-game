e7fae9b47508f14089b4ba7ad80187a4537602cc
Gary Bishop <gary.bishop.unc@gmail.com>
2019-10-15 13:31:17 -0400

    Strip out the runner, just leave the frame.

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
 import "phaser";
-import { WelcomeScene } from "./welcomeScene";
 import { GameScene } from "./gameScene";
-import { ScoreScene } from "./scoreScene";
 
 const config: Phaser.Types.Core.GameConfig = {
   title: "Runner",
   type: Phaser.AUTO,
-  width: 1334,
-  height: 750,
+  width: 512,
+  height: 512,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
   parent: "game",
-  scene: [WelcomeScene, GameScene, ScoreScene],
+  scene: [GameScene],
   physics: {
     default: "arcade",
     arcade: {
@@ -23,12 +21,12 @@ const config: Phaser.Types.Core.GameConfig = {
   backgroundColor: "#444444"
 };
 
-export class StarfallGame extends Phaser.Game {
+export class MyGame extends Phaser.Game {
   constructor(config: Phaser.Types.Core.GameConfig) {
     super(config);
   }
 }
 
 window.onload = () => {
-  var game = new StarfallGame(config);
+  var game = new MyGame(config);
 };
